7fde2fffa2a479a7bd11af456afbfef3df439219
Mustafa Özcan <mustafa452017@gmail.com>
2023-09-09 21:53:58 +0300

    add

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:expansion_panel/model/items.dart';
 import 'package:flutter/material.dart';
 
 class ExpansionPanell extends StatefulWidget {
@@ -7,9 +8,68 @@ class ExpansionPanell extends StatefulWidget {
   State<ExpansionPanell> createState() => _ExpansionPanellState();
 }
 
+final List<Item> _data = newPanel(_sorular.length);
+final List<String> _sorular = [
+  'Ad Soyad',
+  'Meslek',
+  'Doğum Tarihi',
+  'E-mail',
+  'Cep Telefonu',
+  'Cinsiyet',
+];
+List<Item> newPanel(int numberItem) {
+  return List.generate(numberItem, (index) {
+    return Item(headerValue: '${_sorular[index]}');
+  });
+}
+
 class _ExpansionPanellState extends State<ExpansionPanell> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Expansion Panel List Kullanımı'),
+      ),
+      body: _data.isNotEmpty
+          ? SingleChildScrollView(
+              child: Container(
+                margin: EdgeInsets.all(10),
+                child: ExpansionPanelList(
+                  expansionCallback: (panelIndex, isExpanded) {
+                    setState(() {
+                      _data[panelIndex].isExapanded = !isExpanded;
+                    });
+                  },
+                  children: _data.map((Item item) {
+                    return ExpansionPanel(
+                      headerBuilder: (context, isExpanded) {
+                        return ListTile(
+                          title: Text(item.headerValue),
+                        );
+                      },
+                      body: ListTile(
+                        title: Text('Flutter Developer'),
+                        trailing: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _data.remove(item);
+                            });
+                          },
+                          child: Icon(Icons.delete),
+                        ),
+                      ),
+                      isExpanded: item.isExapanded,
+                    );
+                  }).toList(),
+                ),
+              ),
+            )
+          : Center(
+              child: Text(
+                'Veri Yok',
+                style: Theme.of(context).textTheme.headline3,
+              ),
+            ),
+    );
   }
 }
---
+++ lib/model/items.dart
@@ -0,0 +1,9 @@
+class Item {
+  String headerValue;
+  bool isExapanded;
+
+  Item({
+    required this.headerValue,
+    this.isExapanded = false,
+  });
+}
